bbba7f52c4aac47867fb5a45494e69a0bb232a10
Francisco París <franciscoparisalbero@gmail.com>
2019-02-21 18:09:54 +0100

    primer commit

---
+++ primerEjercicio/src/primerEjercicio/HorasTrabajo.java
@@ -0,0 +1,20 @@
+package primerEjercicio;
+//commit
+import java.util.Scanner;
+
+public class HorasTrabajo {
+	public static void main(String[] args) {
+		   
+		Scanner entrada = new Scanner (System.in);
+		   
+		  int horas;
+		  int sueldo;
+		  System.out.println("Cuantos horas has trabajado este mes? ");
+		  horas= entrada.nextInt();
+		   
+		  sueldo = horas*10;
+		   
+		  System.out.println("Tu sueldo de este mes es de: " + sueldo + " €");
+		  entrada.close();
+		 }
+		}
